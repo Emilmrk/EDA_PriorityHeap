1ec13a2a3acc517272ff3b888e1c622a49aa086e
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2023-06-17 10:54:24 -0500

    Update TADheap.java
    unas correcciones simples

--- TADheap.java
+++ TADheap.java
@@ -1,8 +1,7 @@
 package PriorityQueueHeap;
 
-import java.util.ArrayList;
-
 public interface TADheap<E extends Comparable<E>> {
-	void insert(E x,ArrayList<E> array);
-	void remove(ArrayList<E> array);
+	void insert(E x, int priory);
+	void remove();
+	boolean isEmpty();
 }
