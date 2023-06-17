b47ce0db1bee77aa4c29df1cf57216b93d292f5c
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2023-06-17 10:52:46 -0500

    Update priorityQueueHeap.java
    Completamos la clase priorityQueue solo es llamar a los metodos del heap

--- priorityQueueHeap.java
+++ priorityQueueHeap.java
@@ -1,5 +1,23 @@
 package PriorityQueueHeap;
 
-public class priorityQueueHeap {
-
+public class priorityQueueHeap<E extends Comparable<E>> {
+	private heap<E> myHeap;
+	
+	public priorityQueueHeap() {
+		this.myHeap=new heap<E>();
+	}
+	public void Enqueue(E x, int priory) {
+		myHeap.insert(x, priory);
+	}
+	public E Dequeue() {
+		E temp = this.myHeap.getElementk(0);
+		this.myHeap.remove();
+		return temp;
+	}
+	public E front() {
+		return this.myHeap.getElementk(0);
+	}
+	public E back() {
+		return this.myHeap.ultimo();
+	}
 }
