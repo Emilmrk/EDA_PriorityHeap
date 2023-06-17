11d56ff4f25c097ee260ceb1a7943625f6b90c66
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2023-06-17 10:55:26 -0500

    Update test.java
    aqui realizamos las pruebas del heap que es el problema principal del ejercicio

--- test.java
+++ test.java
@@ -1,5 +1,30 @@
 package PriorityQueueHeap;
 
 public class test {
-
+	public static void main(String[]args) {
+		//			ArrayList<Integer> numerosArrayList=new ArrayList<Integer>();
+		//			priorityQueueHeap<Integer> miobjetoHeap =new priorityQueueHeap<Integer>();
+		heap<Integer> myHeap = new heap<Integer>();
+		myHeap.insert(10, 0);//0
+		myHeap.insert(20, 0);//1
+		myHeap.insert(45, 1);//1
+		myHeap.insert(56, 0);//2
+		myHeap.insert(23, 1);//2
+		myHeap.insert(2, 1);//2
+		myHeap.insert(6, 0);//2
+		myHeap.insert(24, 0);//3
+		myHeap.insert(15, 1);//3
+		myHeap.insert(14, 2);//3
+		System.out.println(myHeap);
+		myHeap.remove();
+		System.out.println(myHeap);
+		myHeap.remove();
+		myHeap.remove();
+		myHeap.remove();
+		System.out.println(myHeap);
+		myHeap.insert(50, 2);
+		myHeap.insert(53, 0);;
+		System.out.println(myHeap);
+	}
 }
+
